08db9d2836d7c26824bf4bec35f1ec7111908437
Anggra <anggrapmb@gmail.com>
2022-03-31 16:15:15 +0700

    Experiment increment decrement

---
+++ app/src/main/java/com/example/increment/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.increment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button inc, dec;
+    TextView skor, total;
+    int count, y, z;
+    String x;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inc = findViewById(R.id.increment);
+        dec = findViewById(R.id.decrement);
+        skor = findViewById(R.id.score);
+        total = findViewById(R.id.total);
+        x = skor.getText().toString();
+        y = Integer.parseInt(x);
+
+        inc.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                count++;
+                z = x * count;
+                total.setText(z + "");
+            }
+        });
+
+        dec.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                count--;
+                total.setText(Integer.toString(z));
+
+            }
+        });
+    }
+}
